e80c27d52e082c8dd6d3846819dec6918e719cb5
serafettingunes <54938342+attila40x@users.noreply.github.com>
2021-01-22 22:11:05 +0300

    Add project files.

---
+++ Constructur/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Constructur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer() { Id=1,City="Ankara",FirtName="Ali",LastName="Kaan"};
+            Customer customer2 = new Customer(1,"Kırşehir","Can","Kaya");
+            Console.WriteLine(customer2.City);
+        }
+    }
+    class Customer
+    {
+
+        public Customer(int id,string city,string firstname,string lastname)
+        {
+            Id = id;
+            City = city;
+            FirtName = firstname;
+            LastName = lastname;
+        }
+
+        public Customer()
+        {
+            Console.WriteLine("Yapıcı Blok");
+        }
+        public int Id { get; set; }
+        public string FirtName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
